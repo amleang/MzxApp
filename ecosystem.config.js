f953500989d162e10b5502182255b8d42ef2f827
少游[赵安良] <E20150604@jzez.com>
2017-05-31 15:39:02 +0800

    upate ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'Mzx',
+      script    : './bin/www',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'zal',
+      host : '192.168.184.129',
+      ref  : 'origin/master',
+      repo : 'git@github.com:amleang/Mzx.git',
+      path : '/home/zal/www/production',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.json --env production'
+    
+    }
+  }
+};
